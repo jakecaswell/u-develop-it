fd1058c3f22721f0bbb1303cb9fb39689895dbbe
jcaswell <jakecaswell88@gmail.com>
2021-11-14 15:15:46 -0500

    fixed api routes and added join statements for database tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+Drop Table If Exists candidates;
+Drop Table If Exists parties;
+
 Create Table parties (
     id Integer AUTO_INCREMENT PRIMARY KEY,
     name varChar(50) NOT NULL,
@@ -8,5 +11,7 @@ CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    Constraint fk_party FOREIGN KEY (party_id) References parties(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,38 +22,94 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 )
 
-// // returns all the data from the candidates table
-// db.query(`Select * From candidates`, (err, rows) => {
-//     console.log(rows);
-// })
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name 
+                 AS party_name 
+                 FROM candidates 
+                 LEFT JOIN parties 
+                 ON candidates.party_id = parties.id`;
 
-// // get a single candidate 
-// db.query(`Select * From candidates Where id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// })
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
 
-// // delete a candidate
-// db.query(`Delete From candidates Where id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+        res.json({
+            message: "success",
+            data: rows
+        });
+    })
+    
+})
+// get a single candidate 
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name 
+                 AS party_name 
+                 FROM candidates 
+                 LEFT JOIN parties 
+                 ON candidates.party_id = parties.id 
+                 WHERE candidates.id = ?`;
+    const params = [req.params.id]
 
-// //create a candidate
-// const sql = `Insert Into candidates (id, first_name, last_name, industry_connected)
-//                 Values (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(500).json({error: err.message})
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+})
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+
+// delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+        }
+        res.json({
+        message: 'success',
+        data: body
+        });
+    });
+});
 
 // {CATCHALL ROUTE} default response for any other request (not found) i.e. 'localhost3001/:whatever' => will come back as an http error {CATCHALL ROUTE}
 app.use((req, res) => {
